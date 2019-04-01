4e5c9d42db0596c22bce6f03e7068541c45059b0
Koushik Dutta <koushd@gmail.com>
2019-04-01 11:28:07 -0700

    manage multiple doors

--- src/main.js
+++ src/main.js
@@ -21,9 +21,60 @@ if (!password) {
 }
 
 function GarageController() {
-  this.state = deviceManager.getDeviceState();
+  this.devices = {};
+
   this.ensureLogin()
-  .catch();
+  .then(() => {
+    var devices = [];
+    var payload = {
+        devices,
+    };
+    return this.account.getDevices([3, 7, 17])
+    .then((result) => {
+      if (!result) {
+        log.e('Unable to query MyQ service. Are your "username" and "password" correct?');
+        return;
+      }
+
+      log.i(`device query: ${JSON.stringify(result)}`);
+      if (!result) {
+        log.e('Unable to query MyQ service. Are your "username" and "password" correct?');
+        return;
+      }
+      result = result.devices;
+      for (var r of result) {
+        if (r.typeId == 3) {
+          var info = {
+            name: r.name,
+            // native id is a number. make sure we pass a string.
+            nativeId: r.id.toString(),
+            interfaces: ['OnOff', 'Refresh'],
+            events: ['OnOff'],
+            type: 'Light',
+          }
+          this.devices[info.nativeId] = new GarageLight(this, info);
+        }
+        else if (r.typeId == 7 || r.typeId == 17) {
+          var info = {
+            name: r.name,
+            // native id is a number. make sure we pass a string.
+            nativeId: r.id.toString(),
+            interfaces: ['Entry', 'Refresh'],
+            events: ['Entry'],
+            type: 'Entry',
+          }
+          this.devices[info.nativeId] = new GarageDoor(this, info);
+        }
+        else {
+          continue;
+        }
+
+        devices.push(info);
+      }
+
+      deviceManager.onDevicesChanged(payload);
+    });
+  });
 }
 
 GarageController.prototype.ensureLogin = function() {
@@ -36,132 +87,105 @@ GarageController.prototype.ensureLogin = function() {
   
   return account.login()
   .then((result) => {
+    if (result.returnCode !== 0) {
+      throw new Error(JSON.stringify(result));
+    }
     log.i(`login result: ${JSON.stringify(result)}`);
     this.account = account;
     this.loginTokenTime = Date.now();
 
-    try {
-      this.deviceId = scriptSettings.getInt('deviceId');
-      if (this.deviceId) {
-        log.i(`controlling garage door: ${this.deviceId}`);
-        // all configured successfully, can wait for commands now.
-        return Promise.resolve(this.account);
-      }
-      log.i('No "deviceId" Script Setting found. Searching for default door');
-    }
-    catch (e) {
-      log.e('The existing "deviceId" script configuration value was invalid: ' + e);
-      throw e;
-    }
-
-    log.i('The "deviceId" script configuration was not provided, listing devices to determine a default door.');
-  
-    return account.getDevices([7, 17])
-    .then((result) => {
-      log.i(`device query: ${JSON.stringify(result)}`);
-      if (!result) {
-        log.e('Unable to query MyQ service. Are your "username" and "password" correct?');
-        return;
-      }
-      result = result.devices;
-      if (result.length == 0) {
-        log.e('No doors found.');
-        return Promise.reject();
-      }
-      if (result.length != 1) {
-        log.e('Multiple doors were found. The "deviceId" script configuration value must be provided from one of the following:')
-        for (var i = 0; i < result.length; i++) {
-          var r = result[i];
-          log.e(`${r.id}: ${r.name}`);
-        }
-        return Promise.reject();
-      }
-  
-      var r = result[0];
-      log.i(`Door found. Setting "deviceId" script configuration value to ${r.id}: ${r.name}`);
-      scriptSettings.putInt('deviceId', r.id);
-      this.deviceId = r.id;
-
-      return this.account;
-    })
+    return this.account;
   })
-  .then(() => this.refresh())
   .catch((err) => {
     log.e('Error logging in. Are the "username" and/or "password" script configuration values correct?\n' + err);
     throw err;
   });
 };
 
-GarageController.prototype.closeEntry = function() {
-  if (!this.account) {
-    log.e('could not close garage door, account login failed');
-    return;
-  }
+GarageController.prototype.getDevice = function(nativeId) {
+  return this.devices[nativeId];
+}
 
-  if (!this.deviceId) {
-    log.e('no "deviceId" script setting was found or inferred.')
-    return;
-  }
+function GarageDoor(controller, info) {
+  this.controller = controller;
+  this.info = info;
+  setImmediate(() => this.state = deviceManager.getDeviceState(info.nativeId));
+}
 
-  this.ensureLogin()
-  .then(() => this.account.setDoorState(this.deviceId, 0))
-  .then((result) => {
-    // command success
-    log.i('garage door closed');
-    log.i(JSON.stringify(result));
-  })
-  .catch((err) => {
-    log.e('garage door close failed: ' + err);
-  })
-  .then(() => this.refresh());
-};
+function doorStateCommand(state) {
+  return function() {
+    this.controller.ensureLogin()
+    .then(() => this.controller.account.setDoorState(this.info.nativeId, state))
+    .then((result) => {
+      log.i(JSON.stringify(result));
+    })
+    .catch((err) => {
+      log.e('garage door command failed: ' + err);
+    })
+    .then(() => this.refresh());
+    }
+}
 
-GarageController.prototype.openEntry = function() {
-  if (!this.account) {
-    log.e('could not close, account login failed');
-    return;
-  }
+GarageDoor.prototype.closeEntry = doorStateCommand(0);
+GarageDoor.prototype.openEntry = doorStateCommand(1);
 
-  if (!this.deviceId) {
-    log.e('no "deviceId" script setting was found or inferred.')
-    return;
-  }
-
-  this.ensureLogin()
-  .then(() => this.account.setDoorState(this.deviceId, 1))
-  .then((result) => {
-    log.i('garage door opened');
-    log.i(JSON.stringify(result));
-  })
-  .catch((err) => {
-    log.e('garage door open failed: ' + err);
-  })
-  .then(() => this.refresh());
-};
-
-GarageController.prototype.getRefreshFrequency = function() {
+GarageDoor.prototype.getRefreshFrequency = function() {
   return 60;
 };
 
-GarageController.prototype.refresh = function() {
-  if (!this.account) {
-    return;
-  }
-
-  this.ensureLogin()
-  .then(() => this.account.getDoorState(this.deviceId))
+GarageDoor.prototype.refresh = function() {
+  this.controller.ensureLogin()
+  .then(() => this.controller.account.getDoorState(this.info.nativeId))
   .then((result) => {
     log.i(`Refresh: ${JSON.stringify(result)}`);
     if (result.doorState !== undefined) {
       this.state.entryOpen = result.doorState !== 2;
     }
-    else {
-      delete this.account;
-    }
   })
   .catch((err) => {
     log.e(`error getting door state: ${err}`);
   });
+};
+
+function GarageLight(controller, info) {
+  this.controller = controller;
+  this.info = info;
+  setImmediate(() => this.state = deviceManager.getDeviceState(info.nativeId));
+}
+
+function lightStateCommand(state) {
+  return function() {
+    this.controller.ensureLogin()
+    .then(() => this.controller.account.setLightState(this.info.nativeId, state))
+    .then((result) => {
+      log.i(JSON.stringify(result));
+    })
+    .catch((err) => {
+      log.e('light command failed: ' + err);
+    })
+    .then(() => this.refresh());
+    }
+}
+
+GarageLight.prototype.turnOn = lightStateCommand(1);
+GarageLight.prototype.turnOff = lightStateCommand(0);
+
+GarageLight.prototype.refresh = function() {
+  this.controller.ensureLogin()
+  .then(() => this.controller.account.getLightState(this.info.nativeId))
+  .then((result) => {
+    log.i(`Refresh: ${JSON.stringify(result)}`);
+    if (result.lightState !== undefined) {
+      this.state.on = result.lightState !== 0;
+    }
+  })
+  .catch((err) => {
+    log.e(`error getting light state: ${err}`);
+  });
+};
+
+GarageLight.prototype.getRefreshFrequency = function() {
+  return 60;
 };
 
 export default new GarageController();
